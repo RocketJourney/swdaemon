ac03cdf4ad2a6222d074e91e169a3391c488b2bd
misaelpc <misaelpcgm@gmail.com>
2015-08-12 17:16:53 -0500

    first commit

---
+++ sw_daemon.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"github.com/codegangsta/cli"
+	"os"
+	"time"
+)
+
+func main() {
+
+	messages := make(chan string)
+	app := cli.NewApp()
+	app.Name = "swdaemon"
+	app.Usage = "Checkin Daemon"
+
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "club",
+			Value: "1",
+			Usage: "club Identifier where daemon its running",
+		},
+	}
+	app.Action = func(c *cli.Context) {
+		println("Starting Daemon for club id: ", c.String("club"))
+	}
+
+	go checkIn()
+
+	app.Run(os.Args)
+	msg := <-messages
+	println("Stopping Daemon")
+	println(msg)
+}
+
+func checkIn() {
+	for {
+		println("check-in")
+		delay := (time.Second * time.Duration(1))
+		time.Sleep(delay)
+	}
+}
